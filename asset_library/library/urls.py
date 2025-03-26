976f9dde3bd2a564dfdc7485a772e9b8119f4be8
terskayl <97208428+terskayl@users.noreply.github.com>
2025-03-26 14:43:32 -0400

    Added Asset Detail Page Skeleton
    Added Detail page for assets and skeletons for the forms for adding and updating assets.

--- asset_library/library/urls.py
+++ asset_library/library/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('asset/<str:asset_name>/', views.asset_detail, name='asset_detail'),
 ]
--- asset_library/library/views.py
+++ asset_library/library/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django .template import loader
-from .models import Asset
+from .models import Asset, Commit, AssetVersion, Keyword
 from django.db.models import Q;
 
 # Create your views here.
@@ -14,5 +14,17 @@ def home(request):
     template = loader.get_template('home.html')
     context = {
         'assets': assets,
+    }
+    return HttpResponse(template.render(context, request))
+
+def asset_detail(request, asset_name):
+    asset = Asset.objects.prefetch_related('keywordsList').get(assetName=asset_name)
+    template = loader.get_template('asset_detail.html')
+    commits = Commit.objects.filter(asset=asset).order_by('-timestamp')
+    versions = AssetVersion.objects.filter(asset=asset).order_by('-versionName')
+    context = {
+        'asset': asset,
+        'commits': commits,
+        'versions': versions,
     }
     return HttpResponse(template.render(context, request))
